3a126e89b9d212c837e25de795615f19f74c42f2
pupillary <71779745+pupillary@users.noreply.github.com>
2022-03-29 11:06:16 +0800

    Create 02_关于True和False.py

---
+++ 爬虫/2022_03_15与03_17_直播_python基础/课上代码/2022_03_15_第一次直播_python基础回顾/02_关于True和False.py
@@ -0,0 +1,40 @@
+# # print(True)
+# # print(False)
+#
+# a = "1"  # 字符串 str
+# b = 1  # 整数  int
+# # print(a + 1)  # 报错
+# # print(b + 1)  # 2
+# # print(type(a))  # <class 'str'>   老虎
+# # print(type(b))  # <class 'int'>   狮子
+#
+# # c = int(a)
+# # print(type(c))  # <class 'int'>
+#
+# # 字符串转化成整数 =>  int(s)
+#
+#
+# t = True
+# print(type(t))  # <class 'bool'>
+#
+# #
+# print(bool(0))   # 0, False   非0, True
+# print(bool(""))  # "",False  非空字符串，True
+# print(bool([]))  # [],False,  非空列表是，True
+# # {}, (), set(), None  =>  False
+# # 其他的全是True
+
+# a = 0
+# if a:  # 如果a不空。 则执行123
+#     print(123)
+# else:
+#     print(456)
+
+# a = ?????
+# if a:  # 这里在判断a有没有东西。
+#     xxxx
+
+# x = ????
+# if x == 2:
+#     pass
+
