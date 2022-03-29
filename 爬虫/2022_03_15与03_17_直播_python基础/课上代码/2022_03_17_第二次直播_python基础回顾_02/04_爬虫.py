ad2241151215b90cddf13508791a6f8ad078421e
pupillary <71779745+pupillary@users.noreply.github.com>
2022-03-29 11:07:18 +0800

    Create 04_爬虫.py

---
+++ 爬虫/2022_03_15与03_17_直播_python基础/课上代码/2022_03_17_第二次直播_python基础回顾_02/04_爬虫.py
@@ -0,0 +1,24 @@
+# import xxx
+
+# 一种语法
+# import re  # re.py  re文件夹的__init__.py
+# import json
+# import time
+# import random
+
+# # 我写的模块
+# import jiamijiemi
+# #
+# jiamijiemi.jiami()
+# jiamijiemi.jiemi()
+# print(jiamijiemi.jay)
+
+# 第二种语法
+
+# 从   某个模块  导入   xxx功能
+# from xxxxx   import xxxxxx
+
+# from jiamijiemi import jiami
+#
+# jiami()
+# jiami()
