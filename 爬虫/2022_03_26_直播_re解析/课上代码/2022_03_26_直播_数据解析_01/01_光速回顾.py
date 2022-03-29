7ef5d70ffaa8e0bd899d0a791fc1e90ec5fa8201
pupillary <71779745+pupillary@users.noreply.github.com>
2022-03-29 11:15:07 +0800

    Create 01_光速回顾.py

---
+++ 爬虫/2022_03_26_直播_re解析/课上代码/2022_03_26_直播_数据解析_01/01_光速回顾.py
@@ -0,0 +1,18 @@
+import requests  # pip install requests
+
+# requests.get(url, params={}, headers={UA})  # 发送get请求
+# requests.post(url, params={}, data={}, headers={}) # 发送post请求
+# response 响应
+# resp
+# resp = requests.get()
+# resp.text   # ?? 页面源代码=> 拿到服务器返回的字符串
+# resp.json()  # json => dict  哈哈哈哈哈
+# resp.content  # ?? bytes, 下载 图片, mp3, mp4, ts, exe
+
+# qq.exe => 可执行文件
+# 小姑娘.jpg => 图片
+# 苍老师.avi  => 视频
+# xxx.mp4 => 视频
+
+# 学习.txt  => 视频 2G
+# 苍老师.txt  => 里面的二进制排列没有发生改变. 还是一个视频文件
