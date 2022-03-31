529119619e0a95c0a2ec76a29edc37b3e19414d8
pupillary <71779745+pupillary@users.noreply.github.com>
2022-03-31 12:28:47 +0800

    Create hehe(1).py

---
+++ 爬虫/2022_03_26与2022_03_29_直播_re解析&bs4解析_上/课上代码/2022_03_29_直播_bs4解析_上/2022_03_29_bs4解析/hehe(1).py
@@ -0,0 +1,6 @@
+
+# 页面源代码选择("#cwb")  # <xx id="cwb">
+# 页面源代码选择("div > div > span")
+# 页面源代码选择(".main > .info")
+# 页面源代码选择("#cwb > table > tr > td > .info")
+# .bili-rank-list-video__list > li > div > a
