8407776e7c05bfd7483537261992c4f3294a5c74
pupillary <71779745+pupillary@users.noreply.github.com>
2022-04-24 17:42:54 +0800

    Create 假设是python.py

---
+++ 爬虫/2022_04_23_直播_redis基本使用和面向对象基础/代码/2022_04_23_redis/假设是python.py
@@ -0,0 +1,11 @@
+# dic = {  #  redis
+#     set: string
+#     hset: dict
+#     hmset: dict
+#     xxx: list
+#     SADD: SET
+#     zadd: 有序集合
+#     wife: {name: 章子怡, age: 18}
+#     children: [diyig, erge]
+#
+# }
