ef3f139fb439a72600a6173eb40a24aada951bbd
pupillary <71779745+pupillary@users.noreply.github.com>
2022-05-26 06:11:12 +0800

    Create hello.py

---
+++ 爬虫/2022_05_25_javascript_01/代码/2022_05_24_javascript_01/hello.py
@@ -0,0 +1,35 @@
+# print("helloworld")
+
+# a, b = "哼哈"  # unpack 解包
+# print(a)  # ?
+# print(b)  # ?
+
+# a = "123"  # 能咬人的东北虎
+# b = 123  # 照片上的东北虎
+# print(a == b)
+
+# s = "123456"
+# int(s)
+
+# s = 2748
+# print(hex(s))  # 0xabc
+# print(format(s, "x"))  #abc
+
+# s = "abc"
+# print(int(s, 16))  # 2748
+
+# s = 0xabc
+# print(type(s))
+
+# print(ord("e"))  # 对应的ascii 的编码
+# print(ord("中"))  # 20013 unicode码
+#
+# print(chr(20013))
+#
+# a = None
+# print(bool(a))
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
